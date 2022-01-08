bc1d4e5dd6d69beb0f7f70c0c9177d3ae9f28407
Neo <choneo.ch@gmail.com>
2022-01-07 21:31:43 -0500

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,34 +9,46 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const ETH_MAINNET_URL = process.env.ETH_MAINNET_URL;
+const ETH_RINKEBY_URL = process.env.ETH_RINKEBY_URL;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const POLYGON_MAINNET_URL = process.env.POLYGON_MAINNET_URL;
+const POLYGON_MUMBAI_URL = process.env.POLYGON_MUMBAI_URL;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    mainnet: {
+      url: ETH_MAINNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: ETH_RINKEBY_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    matic: {
+      url: POLYGON_MAINNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: POLYGON_MUMBAI_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: "src/types",
+    target: "ethers-v5",
   },
 };
 
